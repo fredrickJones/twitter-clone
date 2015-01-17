325284659dbc5e4cfe8bada8931f3f907e173143
Fredrick Jones <fredrick.w.jones@gmail.com>
2015-01-16 17:06:23 -0700

    updated

--- js/animations.js
+++ js/animations.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 		$('#tweet-controls').show();
 	});
 
-	$('.tweet-compose').on('keyup', function() {
+	$('#tweet-content .tweet-compose').on('keyup', function() {
 		var remaining = 140;
 		var typed = $(this).val();
 		$('#char-count').text(remaining - typed.length);
@@ -13,7 +13,24 @@ $(document).ready(function () {
 		} else {
 			$('#char-count').css('color', 'rgba(153, 153, 153, 1)');
 		};
+		if (typed.length > 140) {
+			$('#tweet-submit').prop('disabled', true);
+		} else {
+			$('#tweet-submit').prop('disabled', false);
+		};
+		//$('#tweet-content .tweet-compose').keypress(function(e) {
+			//var tval = $('.tweet-compose').val(),
+    		//tlength = tval.length,
+        	//set = 141,
+        	//remain = parseInt(set - tlength);
+        	//$('#char-count').text(remain);
+        	//if (remain <= 0 && e.which !== 0 && e.charCode !== 0) {
+        		//$('#tweet-content .tweet-compose').val((tval).substring(0, tlength - 1))
+    		//};
+		//});
 	});
+
+	//$()
 
 
 
