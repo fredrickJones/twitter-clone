199f164be6f1bd3d4a937cd30ee1b094f559ed49
Fredrick Jones <fredrick.w.jones@gmail.com>
2015-01-18 18:55:27 -0700

    update

--- js/animations.js
+++ js/animations.js
@@ -1,6 +1,68 @@
+/* global $ */
 $(document).ready(function () {
+	'use strict';
+
+	var buildTweet = function(fullName, userName, avatar, text) {
+		return $('<div class="tweet">').append(buildTweetContent(fullName, userName, avatar, text));
+	};
+
+	var buildTweetContent = function(fullName, userName, avatar, text) {
+		return $('<div class="content">').append('' +
+			'<img class="avatar" src="' + avatar + '" />' +
+			'<strong class="fullname">' + fullName + '</strong>' +
+			'<span class="username">@' + userName + '</span>' +
+			'<p class="tweet-text">' + text + '</p>' 
+		)
+		.append(buildTweetActions())
+		.append(buildTweetStats())
+		.append(buildTweetReply());
+	};
+
+	var buildTweetActions = function() {
+		return '' +
+			'<div class="tweet-actions">' +
+				'<ul>' +
+					'<li><span class="icon action-reply"></span> Reply</li>' +
+					'<li><span class="icon action-retweet"></span> Retweet</li>' +
+					'<li><span class="icon action-favorite"></span> Favorite</li>' +
+					'<li><span class="icon action-more"></span> More</li>' +
+				'</ul>' +
+			'</div>';
+	};
+
+	var buildTweetStats = function() {
+		return '' +
+			'<div class="stats">' +
+				'<div class="retweets">' +
+					'<p class="num-retweets">0</p>' +
+					'<p>RETWEETS</p>' +
+				'</div>' +
+				'<div class="favorites">' +
+					'<p class="num-favorites">0</p>' +
+					'<p>FAVORITES</p>' +
+				'</div>' +
+				'<div class="users-interact">' +
+					'<div>' +
+						'<img src="img/funwatercat.jpg" />' +
+						'<img src="img/vklimenko.jpg" />' +
+					'</div>' +
+				'</div>' +
+				'<div class="time">' +
+					'1:04 PM - 19 Sep 13' +
+				'</div>' +
+			'</div>';
+	};
+
+	var buildTweetReply = function() {
+		return '' +
+		'<div class="reply">' +
+			'<img class="avatar" src="img/alagoon.jpg" />' +
+			'<textarea class="tweet-compose" placeholder="Reply to @FredrickWJones"/></textarea>' +
+		'</div>';
+	};
+
 	$('.tweet-compose').on('click', function() {
-		$('.tweet-compose').css('height', '5em');
+		$(this).css('height', '5em');
 		$('#tweet-controls').show();
 	});
 
@@ -12,33 +74,22 @@ $(document).ready(function () {
 			$('#char-count').css('color', 'rgba(178, 6, 6, 1)');
 		} else {
 			$('#char-count').css('color', 'rgba(153, 153, 153, 1)');
-		};
+		}
 		if (typed.length > 140) {
 			$('#tweet-submit').prop('disabled', true);
 		} else {
 			$('#tweet-submit').prop('disabled', false);
-		};
-
-		//$('#tweet-content .tweet-compose').keypress(function(e) {
-			//var tval = $('.tweet-compose').val(),
-    		//tlength = tval.length,
-        	//set = 139,
-        	//remain = parseInt(set - tlength);
-        	//$('#char-count').text(remain);
-        	//if (remain <= 0 && e.which !== 0 && e.charCode !== 0) {
-        		//$('#tweet-content .tweet-compose').val((tval).substring(0, tlength - 1))
-    		//};
-		//});
+		}
 	});
 
 
+	$('#tweet-submit').on('click', function(e) {
+		e.preventDefault();
+		//console.log('test tweet');
+		var text = $('#tweet-content .tweet-compose').val();
+		$('#stream').prepend(buildTweet('Fredrick Jones', 'FredrickWJones', 'img/alagoon.jpg', text));
 
-	$('#tweet-submit').on('click', function() {
-		console.log('test tweet');
-		$('#stream').prepend('.tweet');
-		$('#stream .tweet .content').append('#profile-summary', '#tweet-content', '.tweet-actions', '.stats', '.time', '.reply');
+
+		// $('#stream .tweet .content').append('#profile-summary', '#tweet-content', '.tweet-actions', '.stats', '.time', '.reply');
 	});
-
-
-
 });
